21bd73a25e4243069ccb1e4a8381cfc87873ebfe
Long Ho <longho@Long-Hos-MacBook-Pro.local>
2024-03-22 10:23:39 -0500

    first commit

---
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/MonnigMeteoriteCollectionApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monnigmeteoritecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonnigMeteoriteCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonnigMeteoriteCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/monnigmeteoritecollection/MonnigMeteoriteCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monnigmeteoritecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonnigMeteoriteCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
