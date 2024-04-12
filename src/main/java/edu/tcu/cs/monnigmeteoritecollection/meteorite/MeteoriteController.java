5f6e0b7365a2d7671d60496b87671f649da4069b
Jaxon Hill <jaxonhill19@gmail.com>
2024-04-12 10:25:47 -0500

    Begin UC1, set up MeteoriteService and Repository

--- src/main/java/edu/tcu/cs/monnigmeteoritecollection/meteorite/MeteoriteController.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/meteorite/MeteoriteController.java
@@ -1,5 +1,27 @@
 package edu.tcu.cs.monnigmeteoritecollection.meteorite;
 
+import org.springframework.web.bind.annotation.*;
+
+import edu.tcu.cs.monnigmeteoritecollection.system.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("${api.endpoint.base-url}/meteorites?searchTextFromUser")
 public class MeteoriteController {
 
+    final MeteoriteService meteoriteService = new MeteoriteService(null);
+
+    @GetMapping
+    public Result findAllMeteorites() {
+        // List<Wizard> foundWizards = this.wizardService.findAll();
+        List<Meteorite> meteoriteList = this.meteoriteService.findAll();
+        // convert foundWizards to foundWizardDto
+        
+        
+        return new Result(true, StatusCode.SUCCESS, "Find All Success", meteoriteList);
+    }
+    
 }
--- src/main/java/edu/tcu/cs/monnigmeteoritecollection/meteorite/MeteoriteService.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/meteorite/MeteoriteService.java
@@ -1,4 +1,23 @@
 package edu.tcu.cs.monnigmeteoritecollection.meteorite;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class MeteoriteService {
+
+    private final MeteoriteRepository meteoriteRepository;
+
+    public MeteoriteService(MeteoriteRepository meteoriteRepository) {
+        this.meteoriteRepository = meteoriteRepository;
+    }
+
+    public List<Meteorite> findAll() {
+
+        return this.meteoriteRepository.findAll();
+
+    }
 }
