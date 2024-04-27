3a9c6bd5c8008bcca859508ecab8f811f696e247
jaxonhill1 <jaxonhill19@gmail.com>
2024-04-26 19:47:13 -0500

    missed import for CORS handling

--- src/main/java/edu/tcu/cs/monnigmeteoritecollection/samplehistory/SampleHistoryController.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/samplehistory/SampleHistoryController.java
@@ -3,6 +3,8 @@ package edu.tcu.cs.monnigmeteoritecollection.samplehistory;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/edu/tcu/cs/monnigmeteoritecollection/security/AuthController.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/security/AuthController.java
@@ -3,6 +3,7 @@ package edu.tcu.cs.monnigmeteoritecollection.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
