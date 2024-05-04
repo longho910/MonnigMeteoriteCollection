f834d212520419671ab9fe8eec89f8677056a717
jaxonhill1 <jaxonhill19@gmail.com>
2024-05-04 16:58:49 -0500

    Created CorsConfig.java

---
+++ src/main/java/edu/tcu/cs/monnigmeteoritecollection/system/CorsConfig.java
@@ -0,0 +1,19 @@
+package edu.tcu.cs.monnigmeteoritecollection.system;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:8080")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
